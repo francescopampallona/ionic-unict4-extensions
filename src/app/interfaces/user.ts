441f19af6643a1e914d38c4182d4701b64ed3dc5
Francesco Pampallona <53058502+francescopampallona@users.noreply.github.com>
2019-11-29 03:41:05 +0100

    added favourites

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -1,8 +1,16 @@
+import {Tweet} from './tweet';
 export interface User {
     email: string;
     password?: string;
     name: string;
     surname: string;
     _id?: string;
-    _favourites?: Array<string>;
+    _favourites?: Array<Tweet>;
 }
+
+export interface Favourite{
+    favourite: string;
+
+}
+
+ 
